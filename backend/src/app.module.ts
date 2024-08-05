b4230303df16ec23690d825a59fa651fb52bb1bc
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-05 20:42:47 -0300

    missing connection to db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -15,8 +15,6 @@ import { AuthService } from './modules/auth/auth.service';
       type: 'postgres',
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT, 10),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true, // Set to false in production
