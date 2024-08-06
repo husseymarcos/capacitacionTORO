580e06a959328161990dbe6e5a8888a6338c4cbd
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-06 10:38:28 -0300

    sequalize ORM

--- backend-NestJS/src/app.module.ts
+++ backend-NestJS/src/app.module.ts
@@ -1,23 +1,33 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthService } from './modules/auth/auth.service';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true, 
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // Set to false in production
+      autoLoadModels: true, 
+      synchronize: true,    
+      ssl: true,            
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, 
+        },
+      },
+      logging: console.log, 
+      models: [],       
     }),
     UsersModule,
     AuthModule,
