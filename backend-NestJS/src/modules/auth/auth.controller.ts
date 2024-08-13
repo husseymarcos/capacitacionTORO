95b07499db8520e7de65b2e614ec6ce46e1f4087
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-12 23:24:04 -0300

    first implementation, missing testing

--- backend-NestJS/src/modules/auth/auth.controller.ts
+++ backend-NestJS/src/modules/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {Body, Controller, Post, HttpCode, HttpStatus, UseGuards, Get, Request} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 import {SignInDto} from "./singIn.dto";
+import { SetMetadata } from '@nestjs/common';
 
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 // Deberia tener el registrar aca tambien
 
@@ -11,9 +16,16 @@ export class AuthController {
 
     constructor(private authService: AuthService) {}
 
+    @Public()
     @HttpCode(HttpStatus.OK)
     @Post('login')
     signIn(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto.email, signInDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
