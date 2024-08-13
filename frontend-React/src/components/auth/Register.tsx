b8ed2383fe140b003a128188c7e61431223070cb
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-13 13:43:37 -0300

    Update Register.tsx

--- frontend-React/src/components/auth/Register.tsx
+++ frontend-React/src/components/auth/Register.tsx
@@ -68,7 +68,7 @@ export default function Register() {
                       <LockOutlinedIcon />
                   </Avatar>
                   <Typography component="h1" variant="h5">
-                      Sign Up
+                      Register new Account
                   </Typography>
                   <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
                       <Grid container spacing={2}>
