dc906e8eeedc6201e2b390edd44ff12fc1eebb17
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-15 22:56:14 -0300

    refactor location of components

--- frontend-React/src/App.tsx
+++ frontend-React/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home';
+import Home from './components/home/Home';
 import Register from './components/auth/Register';
 import SignIn from './components/auth/SignIn';
 
--- frontend-React/src/components/home/Home.tsx
+++ frontend-React/src/components/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Typography, Box, Grid } from '@mui/material';
-import FeatureCard from '../pages/FeatureCard';
-import Footer from '../pages/Footer';
+import FeatureCard from './FeatureCard';
+import Footer from './Footer';
 import '../styles/Home.css'; 
 
 const Home: React.FC = () => {
@@ -12,15 +12,12 @@ const Home: React.FC = () => {
                     <Typography variant="h2" className="home-title" gutterBottom>
                         Welcome to Your Daily To-Do List
                     </Typography>
-                    <Typography variant="h5" className="home-subtitle" paragraph>
-                        Organize your tasks efficiently and stay on top of your goals with our easy-to-use to-do list app.
-                    </Typography>
                     <Box className="feature-card-grid">
                         <Grid container spacing={4} className="feature-card-item">
                             <Grid item xs={12} sm={6} md={4}>
                                 <FeatureCard
                                     title="Create and Manage Tasks"
-                                    description="Add new tasks, categorize them, and mark them as completed with ease."
+                                    description="Add new tasks and mark them as completed with ease."
                                     buttonText="Get Started"
                                     buttonLink="/sign-up"
                                     buttonColor="primary"
